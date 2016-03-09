eaab74f6db8283c9177ffda13d51272f66279a81
zafar <xaphor.emam@gmail.com>
2016-03-09 21:19:35 +0400

    capture the authorized charge

--- src/Start/Charge.php
+++ src/Start/Charge.php
@@ -39,4 +39,46 @@ class Start_Charge {
         $return_data = Start_Request::make_request("charge_list");
         return $return_data;
     }
+
+    /**
+     * capture an already authorized charge for given $chargeID and $data
+     *
+     * @param array $data the data for the transaction, contains amount
+     * @return array the result of the transaction
+     * @throws Start_Error_Authentication if the API Key is invalid
+     * @throws Start_Error_Banking if the card could not be accepted
+     * @throws Start_Error_Processing if the there's a failure from Start
+     * @throws Start_Error_Request if any of the parameters is invalid
+     * @throws Start_Error if there is a general error in the API endpoint
+     * @throws Exception for any other errors
+     */
+    public static function capture($chargeId, $data) {
+        //$return_data = Start_Request::make_request("/charges/". $charge_id . "/capture", $data);
+        //return $return_data;
+        $url = Start::getChargeEndPoint($chargeId, 'capture');
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_CAINFO, Start::getCaPath());
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_USERPWD, Start::getApiKey() . ':');
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = json_decode(curl_exec($ch), true);
+
+    // Check for errors and such.
+    $info = curl_getinfo($ch);
+    $errno = curl_errno($ch);
+    if( $result === false || $errno != 0 ) {
+      // Do error checking
+      throw new Exception(curl_error($ch));
+    } else if($info['http_code'] < 200 || $info['http_code'] > 299) {
+      // Got a non-200 error code.
+      Start::handleErrors($result, $info['http_code']);
+    }
+    curl_close($ch);
+
+    return $result;
+
+    }
 }
